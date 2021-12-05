3d24f190d8c18b587e6458fc9d374eaa2a69dd92
Constantine-M <mez365@mail.ru>
2021-12-05 22:26:06 +0300

    8. Реализовать собственную структуру данных - HashMap [#1008 #234647] - final

--- src/main/java/ru/job4j/map/mymap/SimpleMap.java
+++ src/main/java/ru/job4j/map/mymap/SimpleMap.java
@@ -1,6 +1,8 @@
 package ru.job4j.map.mymap;
 
+import java.util.ConcurrentModificationException;
 import java.util.Iterator;
+import java.util.NoSuchElementException;
 
 /**
  * 8. Реализовать собственную структуру данных - HashMap.
@@ -17,13 +19,48 @@ public class SimpleMap<K, V> implements Map<K, V> {
 
     private int modCount = 0;
 
-    private int threshold;
+    /**
+     * Данная переменная поля описывает
+     * кол-во элементов в карте.
+     */
+    private int size = 0;
 
     private MapEntry<K, V>[] table = new MapEntry[capacity];
 
+    /**
+     * Данный метод добавляет пару
+     * ключ-значение в нашу карту (массив).
+     *
+     * Метод максимально упрощен в
+     * учебных целях.
+     * Здесь нет разрешения коллизий.
+     * Здесь не реализуется связный список.
+     *
+     * 1.Расширяем массив.
+     * 2.Находим индекс ячейки, куда
+     * будет помещена пара.
+     * 3.Если ячейка пустая, то
+     * добавляем пару ключ-значение.
+     *
+     * @param key ключ.
+     * @param value значение.
+     * @return true, если пара добавилась,
+     *         false - если нет.
+     */
     @Override
     public boolean put(K key, V value) {
-        return false;
+        boolean rsl;
+        int index = indexFor(hash(key.hashCode()));
+        expand();
+        if ((table[index]) == null) {
+            table[index] = new MapEntry(key, value);
+            rsl = true;
+        } else {
+            rsl = false;
+        }
+        modCount++;
+        size++;
+        return rsl;
     }
 
     /**
@@ -36,51 +73,161 @@ public class SimpleMap<K, V> implements Map<K, V> {
      */
     private int hash(int hashCode) {
         int square = hashCode * hashCode;
-        int middle = (square % 1000000) / 100;
+        int middle = (square % 1234) + 77;
         return middle >> 2;
     }
 
+    /**
+     * Данный метод возвращает индекс
+     * ячейки таблицы.
+     *
+     * Так как хэш код может быть большим,
+     * а исходный массив маленький - нужно
+     * хэш код трансформировать в значения
+     * от 0 до 7 (если емкость таблицы = 8).
+     *
+     * @param hash хэш значение ключа.
+     * @return индекс ячейки.
+     */
     private int indexFor(int hash) {
-        return 0;
+        return hash & (table.length - 1);
     }
 
+    /**
+     * Данный метод возвращает кол-во
+     * элементов в карте.
+     *
+     * @return количество элементов в карте.
+     */
+    @Override
+    public int size() {
+        return size;
+    }
+
+    /**
+     * Данный метод расширяет массив.
+     *
+     * Если емкость массива достигла
+     * значения {@code threshold},
+     * то увеличивем емкость карты в
+     * 2 раза.
+     */
     private void expand() {
-        MapEntry<K, V>[] oldTab = table;
-        int oldCap = (oldTab == null) ? 0 : oldTab.length;
+        int threshold = (int) (LOAD_FACTOR * capacity);
         int newCap = 0;
-        int oldThr = 0;
-        int newThr = 0;
-        if (oldCap > 0) {
-            newThr = oldThr;
-        } else if (oldThr > 0) {
-            newCap = oldThr;
-        } else {
-            newCap = capacity;
-            newThr = capacity * (int) LOAD_FACTOR;
-        }
-        threshold = newThr;
-        MapEntry<K, V>[] newTab = new MapEntry[newCap];
-        if (oldTab != null) {
-            for (int i = 0; i < oldCap; i++) {
-                newTab[i] = oldTab[i];
+        if (size >= threshold) {
+            newCap = capacity * 2;
+            MapEntry<K, V>[] newTab = new MapEntry[newCap];
+            for (int i = 0; i < capacity; i++) {
+                newTab[i] = table[i];
             }
+            capacity = newCap;
+            table = newTab;
         }
-        table = newTab;
     }
 
+    /**
+     * Данный метод возвращает значение
+     * по ключу.
+     *
+     * 1.По ключу находим индекс ячейки.
+     * 2.Возвращаем значение данной ячейки.
+     *
+     * @param key ключ.
+     * @return значение ячейки.
+     */
     @Override
     public V get(K key) {
+        int index = indexFor(hash(key.hashCode()));
+        MapEntry<K, V> element;
+        if (table[index] != null) {
+            element = table[index];
+            return element.value;
+        }
         return null;
     }
 
+    /**
+     * Данный метод удаляет пару
+     * ключ-значение из карты.
+     *
+     * 1.Находим индекс по ключу.
+     * 2.Так как пары записываются
+     * в массив, то чтобы удалить
+     * пару, мы просто обнуляем ячейку.
+     *
+     * @param key ключ.
+     * @return true, если элемент был удален.
+     */
     @Override
     public boolean remove(K key) {
-        return false;
+        boolean rsl;
+        int index = indexFor(hash(key.hashCode()));
+        if (table[index] != null) {
+            table[index] = null;
+            rsl = true;
+            size--;
+            modCount++;
+        } else {
+            rsl = false;
+        }
+        return rsl;
     }
 
     @Override
     public Iterator<K> iterator() {
-        return null;
+        return  new Iterator<K>() {
+
+            int expectedModCount = modCount;
+
+            /**
+             * Данная переменная является указателем
+             * и позволяет перемещаться по ячейкам
+             * карты, которые содержат пару
+             * ключ-значение.
+             */
+            int cursor;
+
+            /**
+             * Итератор запоминает значение
+             * счетчика (modCount) на момент
+             * своего создания (expectedModCount),
+             * а затем на каждой итерации
+             * сравнивает сохраненное значение,
+             * с текущим значением поля modCount.
+             *
+             * Проверяем, чтобы на момент итерирования
+             * не была изменена наша карта,
+             * иначе выбрасываем исключение.
+             * Это называется fail-fast поведение.
+             *
+             * Так как заполнение карты неравномерное,
+             * то пустые ячейки нужно пропускать.
+             */
+            @Override
+            public boolean hasNext() {
+                boolean rsl;
+                if (modCount != expectedModCount) {
+                    throw new ConcurrentModificationException();
+                }
+                while (cursor < table.length) {
+                    if (table[cursor] == null) {
+                        cursor++;
+                    } else {
+                        return true;
+                    }
+                }
+                return false;
+            }
+
+            @Override
+            public K next() {
+                if (!hasNext()) {
+                    throw new NoSuchElementException();
+                }
+                return table[cursor++].key;
+            }
+        };
     }
 
     private static class MapEntry<K, V> {
---
+++ src/test/java/ru/job4j/map/mymap/SimpleMapTest.java
@@ -0,0 +1,127 @@
+package ru.job4j.map.mymap;
+
+import org.junit.Assert;
+import org.junit.Ignore;
+import org.junit.Test;
+
+import java.util.ConcurrentModificationException;
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+import static org.junit.Assert.*;
+
+/**
+ * @author Constantine on 04.12.2021
+ */
+public class SimpleMapTest {
+
+    @Test
+    public void whenPutThenSizeIncrease() {
+        Map<Integer, String> map = new SimpleMap<>();
+        map.put(1, "rum");
+        map.put(2, "whisky");
+        map.put(3, "tequila");
+        map.put(4, "wine");
+        map.put(5, "cognac");
+        map.put(6, "champagne");
+        map.put(7, "scotch");
+        Assert.assertEquals(7, map.size());
+    }
+
+    @Test
+    public void whenPutToTheSameIndexThenFalse() {
+        Map<Integer, String> map = new SimpleMap<>();
+        map.put(2, "whisky");
+        assertFalse(map.put(6, "champagne"));
+    }
+
+    @Test
+    public void whenAddAndGetByCorrectKey() {
+        Map<Integer, String> map = new SimpleMap<>();
+        map.put(1, "rum");
+        map.put(2, "whisky");
+        map.put(3, "tequila");
+        Assert.assertEquals("tequila", map.get(3));
+    }
+
+    @Ignore
+    @Test
+    public void whenAddNullThenGet() {
+        Map<Integer, String> map = new SimpleMap<>();
+        map.put(null, "gin");
+        Assert.assertEquals("gin", map.get(null));
+    }
+
+    @Test
+    public void whenGetByIncorrectKeyThenReturnNull() {
+        Map<Integer, String> map = new SimpleMap<>();
+        map.put(1, "liquor");
+        map.put(2, "wine");
+        assertNull(map.get(3));
+    }
+
+    @Test
+    public void whenRemoveThenSizeDecrease() {
+        Map<Integer, String> map = new SimpleMap<>();
+        map.put(1, "absinthe");
+        map.put(2, "brandy");
+        map.remove(2);
+        Assert.assertEquals(1, map.size());
+    }
+
+    @Test
+    public void whenRemoveThenMustNotBeEmpty() {
+        Map<Integer, String> map = new SimpleMap<>();
+        map.put(1, "absinthe");
+        map.put(2, "cabernet sauvignon");
+        map.put(3, "sauvignon blanc");
+        map.put(4, "whisky");
+        map.remove(2);
+        map.remove(3);
+        Assert.assertEquals("absinthe", map.get(1));
+        Assert.assertEquals("whisky", map.get(4));
+    }
+
+    @Test
+    public void whenGetIteratorFromEmptyMapThenHasNextReturnFalse() {
+        Map<Integer, String> map = new SimpleMap<>();
+        assertFalse(map.iterator().hasNext());
+    }
+
+    @Test(expected =  NoSuchElementException.class)
+    public void whenGetIteratorFromEmptyMapThenNextThrowException() {
+        Map<Integer, String> map = new SimpleMap<>();
+        map.iterator().next();
+    }
+
+    @Test
+    public void whenGetIteratorTwiceThenStartAlwaysFromBeginning() {
+        Map<Integer, String> map = new SimpleMap<>();
+        map.put(1, "rum");
+        Assert.assertEquals(Integer.valueOf(1), map.iterator().next());
+        Assert.assertEquals(Integer.valueOf(1), map.iterator().next());
+    }
+    @Test
+    public void whenCheckIterator() {
+        Map<Integer, String> map = new SimpleMap<>();
+        map.put(1, "rum");
+        map.put(2, "whisky");
+        map.put(3, "cola");
+        Iterator<Integer> iterator = map.iterator();
+        Assert.assertTrue(iterator.hasNext());
+        Assert.assertEquals(Integer.valueOf(1), iterator.next());
+        Assert.assertTrue(iterator.hasNext());
+        Assert.assertEquals(Integer.valueOf(2), iterator.next());
+        Assert.assertTrue(iterator.hasNext());
+        Assert.assertEquals(Integer.valueOf(3), iterator.next());
+        Assert.assertFalse(iterator.hasNext());
+    }
+
+    @Test(expected = ConcurrentModificationException.class)
+    public void whenAddAfterGetIteratorThenMustBeException() {
+        Map<Integer, String> map = new SimpleMap<>();
+        Iterator<Integer> iterator = map.iterator();
+        map.put(1, "wine");
+        iterator.next();
+    }
+}
