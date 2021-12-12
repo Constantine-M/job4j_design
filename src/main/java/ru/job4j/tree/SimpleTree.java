f3455fe4810dd35bbab1f9d152d4403a2155c8f9
Constantine-M <mez365@mail.ru>
2021-12-12 14:46:12 +0300

    1. Создать элементарную структуру дерева [#1711]

---
+++ src/main/java/ru/job4j/tree/SimpleTree.java
@@ -0,0 +1,80 @@
+package ru.job4j.tree;
+
+import java.util.*;
+
+/**
+ * 1. Создать элементарную структуру дерева.
+ * @author Constantine on 07.12.2021
+ */
+public class SimpleTree<E> implements Tree<E> {
+
+    private final Node<E> root;
+
+    public SimpleTree(final E root) {
+        this.root = new Node<>(root);
+    }
+
+    /**
+     * Данный метод находит узел по значению
+     * {@code parent} и добавляет в него
+     * дочерний узел со значением {@code child}.
+     *
+     * Так как метод {@code findBy()} возвращает
+     * {@link Optional}, то код null-safe.
+     *
+     * 1.У родителя дети представлены в виде
+     * списка.
+     * 2.Нужно, чтобы соблюдалось 2 условия:
+     * родитель есть, а дочернего узла нет -
+     * тогда мы добавляем дочерний узел.
+     * Иначе не добавляем.
+     *
+     * @param parent родительский узел.
+     * @param child дочерний узел.
+     */
+    @Override
+    public boolean add(E parent, E child) {
+        boolean rsl = false;
+        Optional<Node<E>> papa = findBy(parent);
+        List<Node<E>> sons = papa.get().children;
+        if (findBy(parent).isPresent()
+                && !findBy(child).isPresent()) {
+            sons.add(new Node<E>(child));
+            rsl = true;
+        }
+        return rsl;
+    }
+
+    @Override
+    public Optional<Node<E>> findBy(E value) {
+        Optional<Node<E>> rsl = Optional.empty();
+        Queue<Node<E>> data = new LinkedList<>();
+        data.offer(this.root);
+        while (!data.isEmpty()) {
+            Node<E> el = data.poll();
+            if (el.value.equals(value)) {
+                rsl = Optional.of(el);
+                break;
+            }
+            data.addAll(el.children);
+        }
+        return rsl;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        SimpleTree<?> that = (SimpleTree<?>) o;
+        return Objects.equals(root, that.root);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(root);
+    }
+}
---
+++ src/main/java/ru/job4j/tree/Tree.java
@@ -0,0 +1,34 @@
+package ru.job4j.tree;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+/**
+ * 1. Создать элементарную структуру дерева.
+ *
+ * Данный интерфейс описыает наше дерево
+ * согласно требованиям задания.
+ *
+ * @author Constantine on 07.12.2021
+ */
+public interface Tree<E> {
+
+    boolean add(E parent, E child);
+
+    Optional<Node<E>> findBy(E value);
+
+    /**
+     * Данный класс описывает узел
+     * дерева. Узел содержит значение
+     * ячейки и ссылки на дочерние узлы.
+     */
+    class Node<E> {
+        final E value;
+        final List<Node<E>> children = new ArrayList<>();
+
+        public Node(E value) {
+            this.value = value;
+        }
+    }
+}
