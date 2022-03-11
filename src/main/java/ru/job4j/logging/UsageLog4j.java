2f3c2e649ebe7fc7d0b89e9e0b831fa07c5e0a62
Constantine-M <mez365@mail.ru>
2022-03-11 21:58:14 +0300

    3. Slf4j - вывод переменных. [#268852]

--- src/main/java/ru/job4j/logging/UsageLog4j.java
+++ src/main/java/ru/job4j/logging/UsageLog4j.java
@@ -34,6 +34,23 @@ import org.slf4j.LoggerFactory;
  * логгеров. Почитать тут:
  * https://refactoring.guru/ru/design-patterns/facade
  *
+ * Оператор плюс (+) для String создает
+ * в памяти новую строку. Это плохо,
+ * потому что в памяти создаются
+ * копии строк.
+ *
+ * Чтобы избежать сложение строк
+ * в slf4j используется шаблон
+ * для подстановки переменных.
+ *
+ * Ниже пример:
+ * {@code void debug(String var1, Object var2, Object var3);}
+ * Первый параметр метода - это
+ * шаблон. Шаблон содержит текст
+ * и отметки, которые заменяются
+ * на параметры.
+ *
+ *
  * @author Constantine on 11.03.2022
  */
 public class UsageLog4j {
@@ -46,5 +63,23 @@ public class UsageLog4j {
         LOG.info("info message");
         LOG.warn("warning message");
         LOG.error("error message");
+        System.out.print(System.lineSeparator());
+        String name = "Consta Mezenin";
+        int age = 256;
+        boolean married = false;
+        char className = 'A';
+        byte cache = 127;
+        long income = 475668345;
+        float cpuPower = 3.5f;
+        double damage = 8549684.1549;
+
+        LOG.debug("User info name : {}, age : {}", name, age);
+        LOG.debug("Name : {}, Married : {}", name, married);
+        LOG.debug("User age : {}, His income : {}", age, income);
+        LOG.debug("CPU power is : {}, Mark : {}", cpuPower, className);
+        LOG.debug("Game character : {}, Damage : {}", name, damage);
+        LOG.debug("L2 cache(Mb) : {}, Mark : {}", cache, className);
+        LOG.debug("CPU power is : {}, Married : {}", cpuPower, married);
+        LOG.debug("Summary income : {}, Married : {}", income, married);
     }
 }
