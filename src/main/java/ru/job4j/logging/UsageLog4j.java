902fc197b21a6a6aa0565980139654f504eda4e5
Constantine-M <mez365@mail.ru>
2022-03-11 20:40:09 +0300

    2. Simple Loggin Facade 4 Java. [#268849]

--- src/main/java/ru/job4j/logging/UsageLog4j.java
+++ src/main/java/ru/job4j/logging/UsageLog4j.java
@@ -1,10 +1,11 @@
 package ru.job4j.logging;
 
-import org.apache.log4j.Logger;
-import org.apache.log4j.LogManager;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * 1. Log4j. Логирование системы.
+ * 2. Simple Loggin Facade 4 Java.
  *
  * Данный класс описывает прицип
  * логирования сообщений.
@@ -14,11 +15,30 @@ import org.apache.log4j.LogManager;
  * а не стандартный (который встроен
  * в Java). Поэтому туть внимательней.
  *
- * @author Constantine on 09.03.2022
+ * Далее представим, что на проекте
+ * 2 команды используют разные библиотеки
+ * для логгирования сообщений.
+ * 1 - исп-ют logback.
+ * 2 - исп-ют log4j.
+ * 2-ой команде придется настроить и log4j,
+ * и logback, чтобы логгирование
+ * работало во всей системе.
+ *
+ * Если команды будут использовать
+ * прослойку slf4j перед своими
+ * системами логгирования, то такой
+ * проблемы не возникнет.
+ *
+ * SLF использует шаблон проектирования
+ * - фасад. Шаблон фасад упрощает АПИ
+ * логгеров. Почитать тут:
+ * https://refactoring.guru/ru/design-patterns/facade
+ *
+ * @author Constantine on 11.03.2022
  */
 public class UsageLog4j {
 
-    private static final Logger LOG = LogManager.getLogger(UsageLog4j.class.getName());
+    private static final Logger LOG = LoggerFactory.getLogger(UsageLog4j.class.getName());
 
     public static void main(String[] args) {
         LOG.trace("trace message");
