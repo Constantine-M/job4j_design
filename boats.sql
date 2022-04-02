0a30c9e6c3be30133d1348271ad383d20847d42c
Constantine <70433548+Constantine-M@users.noreply.github.com>
2022-04-02 16:44:32 +0300

    1. Inner join [#373617 #282936] - fix #1

--- boats.sql
+++ boats.sql
@@ -1,66 +1,89 @@
-create table yachttype
+CREATE TABLE yachttype
 (
-id serial primary key,
+id serial PRIMARY KEY,
 name varchar(255)
 );
 
-create table yachts
+CREATE TABLE yachts
 (
-id serial primary key,
+id serial PRIMARY KEY,
 name varchar(255),
 lengths numeric,
 engine boolean,
 yachttype_id int references yachttype(id)
 );
 
-insert into yachttype (name) values ('парусная');
-insert into yachttype (name) values ('моторная');
-insert into yachttype (name) values ('круизная');
-insert into yachttype (name) values ('спортивная');
-insert into yachttype (name) values ('прогулочная');
-insert into yachttype (name) values ('суперяхта');
-insert into yachttype (name) values ('мегаяхта');
+INSERT INTO yachttype (name) VALUES ('парусная');
+INSERT INTO yachttype (name) VALUES ('моторная');
+INSERT INTO yachttype (name) VALUES ('круизная');
+INSERT INTO yachttype (name) VALUES ('спортивная');
+INSERT INTO yachttype (name) VALUES ('прогулочная');
+INSERT INTO yachttype (name) VALUES ('суперяхта');
+INSERT INTO yachttype (name) VALUES ('мегаяхта');
 
-insert into yachts (name, lengths, engine, yachttype_id)
-values ('Oceanis 34.1', 11, false, 1);
-insert into yachts (name, lengths, engine, yachttype_id)
-values ('Oceanis 34.1', 11, false, 3);
-insert into yachts (name, lengths, engine, yachttype_id)
-values ('First 27', 8, false, 1);
-insert into yachts (name, lengths, engine, yachttype_id)
-values ('First 27', 8, false, 4);
-insert into yachts (name, lengths, engine, yachttype_id)
-values ('Yacht 62', 20, false, 1);
-insert into yachts (name, lengths, engine, yachttype_id)
-values ('Velvette 23', 7, true, 2);
-insert into yachts (name, lengths, engine, yachttype_id)
-values ('Velvette 23', 7, true, 5);
-insert into yachts (name, lengths, engine, yachttype_id)
-values ('Velvette 29 Envy', 9, true, 2);
-insert into yachts (name, lengths, engine, yachttype_id)
-values ('Velvette 29 Envy', 9, true, 3);
-insert into yachts (name, lengths, engine, yachttype_id)
-values ('Velvette 29 Envy', 9, true, 4);
-insert into yachts (name, lengths, engine, yachttype_id)
-values ('Galeon 780 Crystal', 25, true, 2);
-insert into yachts (name, lengths, engine, yachttype_id)
-values ('Galeon 780 Crystal', 25, true, 6);
-insert into yachts (name, lengths, engine, yachttype_id)
-values ('Wally Ace 42', 42, true, 2);
-insert into yachts (name, lengths, engine, yachttype_id)
-values ('Wally Ace 42', 42, true, 7);
+INSERT INTO yachts (name, lengths, engine, yachttype_id)
+VALUES ('Oceanis 34.1', 11, false, 1);
+INSERT INTO yachts (name, lengths, engine, yachttype_id)
+VALUES ('Oceanis 34.1', 11, false, 3);
+INSERT INTO yachts (name, lengths, engine, yachttype_id)
+VALUES ('First 27', 8, false, 1);
+INSERT INTO yachts (name, lengths, engine, yachttype_id)
+VALUES ('First 27', 8, false, 4);
+INSERT INTO yachts (name, lengths, engine, yachttype_id)
+VALUES ('Yacht 62', 20, false, 1);
+INSERT INTO yachts (name, lengths, engine, yachttype_id)
+VALUES ('Velvette 23', 7, true, 2);
+INSERT INTO yachts (name, lengths, engine, yachttype_id)
+VALUES ('Velvette 23', 7, true, 5);
+INSERT INTO yachts (name, lengths, engine, yachttype_id)
+VALUES ('Velvette 29 Envy', 9, true, 2);
+INSERT INTO yachts (name, lengths, engine, yachttype_id)
+VALUES ('Velvette 29 Envy', 9, true, 3);
+INSERT INTO yachts (name, lengths, engine, yachttype_id)
+VALUES ('Velvette 29 Envy', 9, true, 4);
+INSERT INTO yachts (name, lengths, engine, yachttype_id)
+VALUES ('Galeon 780 Crystal', 25, true, 2);
+INSERT INTO yachts (name, lengths, engine, yachttype_id)
+VALUES ('Galeon 780 Crystal', 25, true, 6);
+INSERT INTO yachts (name, lengths, engine, yachttype_id)
+VALUES ('Wally Ace 42', 42, true, 2);
+INSERT INTO yachts (name, lengths, engine, yachttype_id)
+VALUES ('Wally Ace 42', 42, true, 7);
+INSERT INTO yachts (name, lengths, engine, yachttype_id)
+VALUES ('Damen SeaXplorer 62', 65, true, 7);
+INSERT INTO yachts (name, lengths, engine, yachttype_id)
+VALUES ('Scheherazade', 140, true, 7);
+INSERT INTO yachts (name, lengths, engine, yachttype_id)
+VALUES ('BLACK PEARL', 106, false, 7);
+INSERT INTO yachts (name, lengths, engine, yachttype_id)
+VALUES ('BLACK PEARL', 106, false, 1);
+INSERT INTO yachts (name, lengths, engine, yachttype_id)
+VALUES ('Sailing Yacht A', 142, false, 1);
+INSERT INTO yachts (name, lengths, engine, yachttype_id)
+VALUES ('Sailing Yacht A', 142, false, 7);
 
-select yach.name, yatype.name
-from yachts as yach
-join yachttype as yatype
-on yach.yachttype_id = yatype.id
+SELECT
+	boat.name,
+	btype.name
+FROM yachts as boat
+INNER JOIN yachttype as btype ON
+	boat.yachttype_id = btype.id;
 
-select yach.name, yatype.name, yach.lengths
-from yachts as yach
-join yachttype as yatype
-on yach.lengths > 12
+SELECT
+	boat.name,
+	btype.name,
+	boat.lengths
+FROM yachts as boat
+INNER JOIN yachttype as btype ON
+	boat.yachttype_id = btype.id
+WHERE boat.lengths > 12;
 
-select yach.name, yatype.name, yach.engine, yach.yachttype_id
-from yachts as yach
-join yachttype as yatype
-on yach.yachttype_id = 7
+SELECT
+	boat.name,
+	btype.name,
+	boat.lengths,
+	boat.engine
+FROM yachts as boat
+INNER JOIN yachttype as btype ON
+	boat.yachttype_id = btype.id
+WHERE boat.yachttype_id = 7;
