9e35e5a94e0056057446a910bebe5474469e2424
Constantine-M <mez365@mail.ru>
2022-09-06 23:13:55 +0300

    1. Реализация кеша на SoftReference [#1592] - fix #1

--- src/main/java/ru/job4j/gc/cache/DirFileCache.java
+++ src/main/java/ru/job4j/gc/cache/DirFileCache.java
@@ -1,5 +1,12 @@
 package ru.job4j.gc.cache;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
 /**
  * 1. Реализация кеша на SoftReference.
  *
@@ -10,11 +17,19 @@ package ru.job4j.gc.cache;
  */
 public class DirFileCache extends AbstractCache<String, String> {
 
+    private static final Logger LOG = LoggerFactory.getLogger(DirFileCache.class.getName());
+
     /**
+     * Данное поле описывает кешируемую
+     * папку, в которой мы будем кешировать
+     * отдельно взятые файлы.
+     *
      * Относительный путь работает, если
      * использовать точку.
-     * . означает ту же папку, что и текущий контекст.
-     * .. означает родительскую папку текущего контекста.
+     * "." - означает ту же папку, что и
+     * текущий контекст.
+     * ".." - означает родительскую папку
+     * текущего контекста.
      */
     private final String cachingDir;
 
@@ -22,9 +37,35 @@ public class DirFileCache extends AbstractCache<String, String> {
         this.cachingDir = cachingDir;
     }
 
+    /**
+     * Данный метод загружает в память
+     * информацию из указаного файла.
+     * Далее оно будет загружено в кеш.
+     *
+     * Метод {@link Path#of(String, String...)}
+     * чем-то похож на метод
+     * {@link Path#resolve(String)} -
+     * каждый раз, добавляя файл через
+     * запятую, мы формируем более полный
+     * путь. Пример:
+     * Path.of(String bar, String foo, String buz)
+     * -> bar/foo/buz
+     * bar.resolve(String foo) ->
+     * bar/foo...Надеюсь, что понял.
+     *
+     * @param key ключ получения объекта кеша.
+     * @return текст, загруженный в память
+     * из файла.
+     */
     @Override
     protected String load(String key) {
-
-        return null;
+        System.out.println("Load file to cache..");
+        String content = "";
+        try {
+            content = Files.readString(Path.of(cachingDir, key));
+        } catch (IOException e) {
+            LOG.error("Error was write to log", e);
+        }
+        return content;
     }
 }
--- src/main/java/ru/job4j/gc/cache/menu/Emulator.java
+++ src/main/java/ru/job4j/gc/cache/menu/Emulator.java
@@ -1,9 +1,69 @@
 package ru.job4j.gc.cache.menu;
 
+import ru.job4j.gc.cache.DirFileCache;
+
+import java.util.Scanner;
+
 /**
  * 1. Реализация кеша на SoftReference.
  *
  * @author Constantine on 28.08.2022
  */
 public class Emulator {
+
+    public static final String SELECT = "Выберите меню: ";
+
+    public static final String DIRECTORY = "Укажите директорию: ";
+
+    public static final String EXIT = "Конец работы";
+
+    public static final String MENU = """
+                1. Указать кэшируемую директорию (файл).
+                2. Загрузить содержимое файла в кэш.
+                3. Получить содержимое файла из кэша.
+                Введите любое другое число для выхода.
+            """;
+
+    /**
+     * Данное поле описывает выбор
+     * пользователя. Эта переменная
+     * запоминает, какой файл указал
+     * пользователь, чтобы в дальнейшем
+     * использовать это в процессе
+     * кеширования или получения
+     * содержимого файла.
+     */
+    private static String userChoiceDir;
+
+    private static void run(DirFileCache dirFileCache, Scanner scanner) {
+        boolean run = true;
+        while (run) {
+            System.out.println(MENU);
+            System.out.print(SELECT);
+            int userChoice = Integer.parseInt(scanner.nextLine());
+            System.out.println("Выбран пункт - " + userChoice);
+            switch (userChoice) {
+                case 1 -> {
+                    System.out.print(DIRECTORY);
+                    userChoiceDir = scanner.nextLine();
+                }
+                case 2 -> {
+                    dirFileCache.put(userChoiceDir, "something");
+                }
+                case 3 -> {
+                    System.out.println(dirFileCache.get(userChoiceDir));
+                }
+                default -> {
+                    run = false;
+                    System.out.println(EXIT);
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        DirFileCache dirFileCache = new DirFileCache("C:\\projects\\job4j_design\\data\\");
+        run(dirFileCache, scanner);
+    }
 }
