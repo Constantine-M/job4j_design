aaf934d7aea7db829676f4fdf0f3c0ffb26c7976
Constantine-M <mez365@mail.ru>
2022-10-28 23:51:26 +0300

    1. Принципы Kiss, Dry и Yagni [#238813 #352673] - fix #3

--- src/main/java/ru/job4j/kiss/MaxMin.java
+++ src/main/java/ru/job4j/kiss/MaxMin.java
@@ -11,38 +11,55 @@ import java.util.function.BiPredicate;
  */
 public class MaxMin {
 
+    /**
+     * Если этот метод возвращает отрицательное число,
+     * то текущий объект будет располагаться перед тем,
+     * который передается через параметр. Если метод
+     * вернет положительное число, то, наоборот, после
+     * второго объекта. Если метод возвратит ноль,
+     * значит, оба объекта равны.
+     *
+     * @param value список сравниваемых объектов.
+     * @param comparator компаратор - штатный или самописный.
+     * @return максимальное число/объект.
+     * @param <T> объект
+     */
     public <T> T max(List<T> value, Comparator<T> comparator) {
-        return null;
+        validate(value);
+        return compare(value, (a, b) -> comparator.compare(a, b) < 0);
     }
 
     public <T> T min(List<T> value, Comparator<T> comparator) {
-        return null;
+        validate(value);
+        return compare(value, (a, b) -> comparator.compare(a, b) > 0);
     }
 
     /**
      * Данный метод производит сравнение.
-     * В {@link BiPredicate} поступает 2
-     * аргумента - это наш объект и
-     * компаратор, кторый будет сравнивать.
-     * Получается, нам нужно все таки написать
-     * свой компаратор (просто чтоб был), а в
-     * методах min/max менять на тот, что передаем
-     * в метод.
-     * {@link BiPredicate} без аргументов не
-     * работает. Значит, нам нужно добавить во
-     * входные параметры компаратор, чтобы
-     * потом его можно было поменять на другой.
+     * 1.Мы проходмся по списку.
+     * 2.В {@link BiPredicate} поступает 2
+     * сравниваемых объекта.
+     * 3.Если условие предиката
+     * выполняется, то начальному значению
+     * присваивается новое значение.
+     *
+     * Например, если нужно макс число..
+     * Если текущее число меньше чем
+     * того, с которым сравниваем, то
+     * текущему числу присваиваем значение
+     * числа, с которым сравнивали
+     * (то есть макс число).
      */
-    /*public <T> T uniMethod(List<T> list, BiPredicate<T, Comparator<T>> pred) {
+    public <T> T compare(List<T> list, BiPredicate<T, T> pred) {
         validate(list);
         T result = list.get(0);
         for (T value : list) {
-            if (pred.test(value, missing argument - have to use some Comparator)) {
+            if (pred.test(result, value)) {
                 result = value;
             }
         }
         return result;
-    }*/
+    }
 
     private <T> void validate(List<T> list) {
         if (list.isEmpty()) {
---
+++ src/test/java/ru/job4j/kiss/MaxMinTest.java
@@ -0,0 +1,73 @@
+package ru.job4j.kiss;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.Comparator;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.assertj.core.api.Assertions.*;
+
+class MaxMinTest {
+
+    @Test
+    void whenGetMaxIntValue() {
+        MaxMin value = new MaxMin();
+        List<Integer> list = List.of(1, 3, 5, 7, 10);
+        int result = value.max(list, Integer::compareTo);
+        assertSame(10, result);
+    }
+
+    /**
+     * Видимо, максимальное значение -
+     * это значение ближе к концу
+     * алфавита.
+     */
+    @Test
+    void whenGetMaxStringValue() {
+        MaxMin value = new MaxMin();
+        List<String> list = List.of("OG", "Secret", "Consta", "LGD", "Liquid");
+        String result = value.max(list, String::compareTo);
+        assertSame("Secret", result);
+    }
+
+    /**
+     * Метод instanceOf вызвать не удается.
+     * Необходимо написать тест на
+     * выброс исключения в случае,
+     * если список пустой.
+     */
+    @Test
+    void whenListIsEmptyThenThrowException() {
+        MaxMin value = new MaxMin();
+        List<String> list = List.of();
+        Comparator<String> cmp = String::compareTo;
+        assertThatThrownBy(() -> {
+            throw new IllegalArgumentException("List is empty!"); });
+        /*assertThatThrownBy(value.max(list, cmp)).isInstanceOf(IllegalArgumentException.class);
+        Exception exception = assertThrows(IllegalArgumentException.class () -> {
+            value.max(list, cmp);
+        });*/
+    }
+
+    /**
+     * Минимальное значение -
+     * это значение ближе к началу
+     * алфавита.
+     */
+    @Test
+    void whenGetMinStringValue() {
+        MaxMin value = new MaxMin();
+        List<String> list = List.of("OG", "Secret", "Consta", "LGD", "Liquid");
+        String result = value.min(list, String::compareTo);
+        assertEquals("Consta", result);
+    }
+
+    @Test
+    void whenGetMinIntValue() {
+        MaxMin value = new MaxMin();
+        List<Integer> list = List.of(2, 1, 4, 3);
+        int result = value.min(list, Integer::compareTo);
+        assertEquals(1, result);
+    }
+}
