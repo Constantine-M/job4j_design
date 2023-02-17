53bc5a4f3c567787490562748ac9889b04c405a3
Constantine-M <mez365@mail.ru>
2023-02-17 23:46:44 +0300

    1. Динамическое перераспределение продуктов [#854 #391758]

--- src/main/java/ru/job4j/ood/lsp/storegoods/control/ControlQuality.java
+++ src/main/java/ru/job4j/ood/lsp/storegoods/control/ControlQuality.java
@@ -3,6 +3,7 @@ package ru.job4j.ood.lsp.storegoods.control;
 import ru.job4j.ood.lsp.storegoods.food.Food;
 import ru.job4j.ood.lsp.storegoods.store.Store;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -31,5 +32,24 @@ public class ControlQuality {
                 storage.add(food);
             }
         }
+    }
+
+    /**
+     * Данный метод извлекает все
+     * продукты и распредляет их
+     * заново.
+     * 1.Проходимся по всем хранилищам.
+     * 2.Извлекаем оттуда продкуты и
+     * добавляем в наш список продуктов
+     * для повторного распределения.
+     * 3.Вновь применяем метод
+     * {@link ControlQuality#distribute}.
+     */
+    public void resort() {
+        List<Food> foodToResort = new ArrayList<>();
+        for (Store storage : stores) {
+            foodToResort.addAll(storage.findAll(x -> x.getPrice() > 0));
+        }
+        distribute(foodToResort);
     }
 }
