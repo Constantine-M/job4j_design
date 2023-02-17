97c65e9ab76bafe6d4292149cad9628713612289
Constantine-M <mez365@mail.ru>
2023-02-17 23:14:15 +0300

    1. Создать меню. [#4748] - fix #4

--- src/main/java/ru/job4j/ood/isp/menu/TodoApp.java
+++ src/main/java/ru/job4j/ood/isp/menu/TodoApp.java
@@ -1,6 +1,17 @@
 package ru.job4j.ood.isp.menu;
 
+import java.util.Optional;
+
 public class TodoApp {
+
+    private static final int ADD_TO_ROOT_ACTION = 1;
+
+    private static final int ADD_TO_PARENT_ACTION = 2;
+
+    private static final int CALL_ASSIGNED_ACTION = 3;
+
+    private static final int PRINT_ACTION = 4;
+
 
     public static final String SELECT = "Select menu: ";
 
@@ -18,7 +29,7 @@ public class TodoApp {
                 5. Exit.
             """;
 
-    private static final ActionDelegate DEFAULT_ACTION = System.out::println;
+    private static final ActionDelegate DEFAULT_ACTION = () -> System.out.println("Some action...");
 
     private static String userChoiceChildName;
 
@@ -30,22 +41,27 @@ public class TodoApp {
             System.out.println(MENU);
             int userChoice = Integer.parseInt(input.askStr(SELECT));
             switch (userChoice) {
-                case 1 -> {
+                case ADD_TO_ROOT_ACTION -> {
                     userChoiceChildName = input.askStr(CHILD_ITEM);
                     menu.add(Menu.ROOT, userChoiceChildName, DEFAULT_ACTION);
                 }
-                case 2 -> {
+                case ADD_TO_PARENT_ACTION -> {
                     userChoiceChildName = input.askStr(CHILD_ITEM);
                     userChoiceParentName = input.askStr(PARENT_ITEM);
                     menu.add(userChoiceParentName, userChoiceChildName, DEFAULT_ACTION);
                 }
-                case 3 -> {
+                case CALL_ASSIGNED_ACTION -> {
                     userChoiceChildName = input.askStr(CHILD_ITEM);
-                    menu.select(userChoiceChildName).get()
-                            .getActionDelegate()
-                            .delegate();
+                    Optional<Menu.MenuItemInfo> itemInfo = menu.select(userChoiceChildName);
+                    if (itemInfo.isEmpty()) {
+                        System.out.println("Menu item not found!");
+                    } else {
+                        itemInfo.get()
+                                .getActionDelegate()
+                                .delegate();
+                    }
                 }
-                case 4 -> {
+                case PRINT_ACTION -> {
                     printer.print(menu);
                 }
                 default -> {
